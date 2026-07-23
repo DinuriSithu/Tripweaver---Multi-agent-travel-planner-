d94bd29006483486f403613fef5a875cb02d361b
Dinuri Abeynayake <dinurisithu@gmail.com>
2026-07-23 23:29:46 +0530

    Update mcp_client.py
    Improve MCP service isolation

--- agents/mcp_client.py
+++ agents/mcp_client.py
@@ -2,39 +2,40 @@ import os
 from dotenv import load_dotenv
 from langchain_mcp_adapters.client import MultiServerMCPClient
 
-
 load_dotenv(override=True)
 
 HOTEL_MCP_URL = os.getenv("HOTEL_MCP_URL")
 FLIGHT_MCP_URL = os.getenv("FLIGHT_MCP_URL")
 
-if not HOTEL_MCP_URL:
-    raise RuntimeError(
-        "HOTEL_MCP_URL is not configured in the environment."
+
+async def get_hotel_tools():
+    if not HOTEL_MCP_URL:
+        raise RuntimeError("HOTEL_MCP_URL is not configured in the environment.")
+
+    client = MultiServerMCPClient(
+        {
+            "hotel-service": {"url": HOTEL_MCP_URL, "transport": "streamable_http",}
+        }
     )
-
-
-if not FLIGHT_MCP_URL:
-    raise RuntimeError(
-        "FLIGHT_MCP_URL is not configured in the environment."
-    )
-
-MCP_SERVERS = {
-    "hotel-service": {
-        "url":HOTEL_MCP_URL,
-        "transport": "streamable_http",
-    },
-    "flight-service": {
-        "url":FLIGHT_MCP_URL,
-        "transport": "streamable_http",
-    }
-}
-
-async def get_mcp_tools():
-    client = MultiServerMCPClient(MCP_SERVERS)
 
     tools = await client.get_tools()
+    return {
+        tool.name: tool
+        for tool in tools
+    }
 
+
+async def get_flight_tools():
+    if not FLIGHT_MCP_URL:
+        raise RuntimeError("FLIGHT_MCP_URL is not configured in the environment.")
+
+    client = MultiServerMCPClient(
+        {
+            "flight-service": {"url": FLIGHT_MCP_URL,"transport": "streamable_http",}
+        }
+    )
+
+    tools = await client.get_tools()
     return {
         tool.name: tool
         for tool in tools
